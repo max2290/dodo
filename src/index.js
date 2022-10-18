a95f50d5459952113cd7b9d9c31fff065bce83e9
max2290 <74972411+max2290@users.noreply.github.com>
2022-10-18 16:08:12 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import "./style.css";
-
-const  form = document.querySelector('form');
-const  input = document.querySelector('input');
-
-form.addEventListener('submite',  event => {
-console.log(event);
-    event.preventDefault();
-});
-input.addEventListener("keydown", event => {
-    if (event.key === 't') {
-        input.style.borderColor = "red";
-    }
-    console.log(event);
-} );
+import './style.css'

4bd260c7abd15f08caf8bae34aa918d9d98a5281
max2290 <74972411+max2290@users.noreply.github.com>
2022-11-14 11:59:38 +0100

    a

--- src/index.js
+++ src/index.js
@@ -3,46 +3,110 @@ import './style.css'
  const  ul = document.querySelector('ul');
  const form = document.querySelector('form');
  const  input = document.querySelector("form > input")
-console.log(form, input);
- form.addEventListener('submit', event => {
-     event.preventDefault()
-     const value = input.value;
-     input.value = '';
-     addTodo(value);
-     displayTodo()
- });
 
-const todos = [
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const value = input.value;
+    input.value = '';
+    addTodo(value);
+});
+
+ const todos = [
     {
         text: 'je suis une todo ' ,
-        done: false
+        done: false,
+        editMode: true
     },
     {
-        text: 'fair du Java script',
-        donne: true
+        text: 'faire du JavaScript',
+        done: true,
+        editMode: false
+
     }
 ] ;
+
 const displayTodo = () => {
     const todosNode = todos.map((todo, index) => {
-        return createTodoElement(todo, index);
+        if (todo.editMode) {
+            return createTodoEditElement (todo,index);
+
+        } else {
+            return createTodoElement(todo, index);
+        }
+
     });
     ul.innerHTML = "";
-    ul.append(...todosNode);
+    ul.append(...todosNode)
 };
-const  createTodoElement = (todo, index) => {
-    const li = document.createElement('li');
+const  createTodoElement = (todos, index) => {
+    const li = document.createElement("li");
+    const  buttonDelete = document.createElement("button");
+    buttonDelete.innerHTML= 'suprimer';
+    const  buttonEdit = document.createElement('button');
+    buttonEdit.innerHTML ="EDIT";
+    buttonDelete.addEventListener('click',event => {
+        event.stopPropagation();
+        deleteTodo(index);
+    } );
+    buttonEdit.addEventListener('click',event => {
+        event.stopPropagation()
+        toggleEditMode(index)
+    } )
     li.innerHTML = `
-    <span class="todos" ${ todo.done ? 'done': "" } "></span>
-    <p> ${todo.text}<p> 
-    <button>Supprimer</button>
-    
+    <span class="todo ${ todos.done ? "done": "" } "></span>
+    <p>${todos.text}<p> 
 `;
+    li.addEventListener('click',event => {
+        toggleTodo(index);
+    });
+li.append(buttonEdit,buttonDelete)
     return li ;
 };
-const  addTodo = text =>{
+const  createTodoEditElement    = (todo, index) => {
+const li = document.createElement('li');
+const input = document.createElement('input');
+input.type='text';
+input.value = todo.text;
+const buttonSave = document.createElement('button');
+buttonSave.innerHTML = 'save';
+const buttonCancel = document.createElement('button');
+buttonCancel.innerHTML='cancel';
+buttonCancel.addEventListener("click", event => {
+    event.stopPropagation()
+    toggleEditMode(index);
+})
+    buttonSave.addEventListener("click",event => {
+        editTodo(index, input )
+    } )
+li.append(input,buttonCancel,buttonSave);
+return li ;
+}
+const  addTodo = text => {
     todos.push({
         text,
         done: false
     });
+    displayTodo()
 }
+const deleteTodo = index => {
+    todos.splice(index,1);
+    displayTodo();
+}
+const toggleTodo = index => {
+    todos[index].done = !todos[index].done;
+    displayTodo();
+};
+
+const toggleEditMode =  index => {
+    todos[index].editMode = !todos[index].editMode;
+    displayTodo();
+}
+const editTodo = (index , input ) => {
+    const value = input.value;
+    todos[index].text = value ;
+    todos[index].editMode= false;
+    displayTodo();
+}
+
+
 displayTodo()
